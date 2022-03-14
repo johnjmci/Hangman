8ce88dce802fe88dd3111ee0d1e5f53235689636
johnjmci <mcivorjohnj@gmail.com>
2022-03-14 16:53:20 +0000

    nitial commit

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# Install repository of available words
+
+# Welcome user and ask for input of first name
+
+# Use username to explain rules to user
+
+# As user to press any key to begin
+
+# Display word skeleton and empty gallowes, request first letter guess
+
+# Run code to check if the input is valid - a single letter character from a to z
+
+# Check whether guess is contained in word, if yes then congrat user, no add body part and comiserate
+
+# If lives and letters remain, LOOP code 
+
+# If no lives remain then GAME OVER
+
+# If no letters remain then CONGRATULATIONS
