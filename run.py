a6c1a5dadc8cf04b4930e90490aad6b86dad8d1e
johnjmci <mcivorjohnj@gmail.com>
2022-03-15 10:59:22 +0000

    Insert and format list of posible words

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 # Install repository of available words
+import random
+from words import possible_words
+print(possible_words)
+
+
 
 # Welcome user and ask for input of first name
 
