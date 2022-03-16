c634aae8cda9e9094ecb03191c860c4fc131c835
johnjmci <mcivorjohnj@gmail.com>
2022-03-16 11:41:53 +0000

    Update word list to external list, add welcome message and request for name

--- run.py
+++ run.py
@@ -3,11 +3,10 @@ Hangman
 """
 # Install repository of available words
 import random
-# from allwords import possible_words
-possible_words = ['apple', 'banana', 'pears']
+from allwords import possible_words
+# possible_words = ['apple', 'banana', 'pears']
 
 
-# Welcome user and ask for input of first name
 # name_input = input("What is your name?")
 # print(name_input)
 
@@ -16,6 +15,13 @@ possible_words = ['apple', 'banana', 'pears']
 # Ask user to press any key to begin
 
 
+# Welcome user and ask for input of first name
+print('Welcome to Hangman!\nThis is a fun word game, where you guess the letters of a mystery word.')
+print()
+print('In this version of the game, all words have six letters.')
+print()
+username = input('Before we start, what should I call you? Plesae enter your first name... ')
+
 wrong_answers = 0
 guessed_letters = []
 number_of_lives = 7
@@ -23,37 +29,46 @@ mystery_word = random.choice(possible_words)
 mystery_letters = list(mystery_word)
 wrong_letters = []
 
+
+
+
+
+
+print('The word has {} letters'.format(len(mystery_letters)))
+
+
+
 while wrong_answers < number_of_lives:
-        print("Incorrect answers so far = ", end="")
+        print('Incorrect answers so far: ', end='')
         for letter in wrong_letters:
-                print("{}, ".format(letter), end="")
-        
-        print("Lives remaining = : {}".format(number_of_lives - wrong_answers))
-        user_guess = input("Enter a letter to check if it is in the mystery word.")
+                print('{}, '.format(letter), end='')
+        print()
+        print('Lives remaining: {}'.format(number_of_lives - wrong_answers))
+        user_guess = input('Enter a letter to check if it is in the mystery word...')
 
         while user_guess in guessed_letters or user_guess in wrong_letters:
-                print("You've already guessed that letter, please try again.")
-                user_guess = input("Enter a letter to check if it is in the mystery word.")
+                print('You already madethat guess, please try again')
+                user_guess = input('Enter a letter to check if it is in the mystery word.')
 
         if user_guess not in mystery_letters:
                 wrong_answers += 1
-
                 wrong_letters.append(user_guess)
 
-        print("MYSTERY Word = ", end="")
+        print('MYSTERY Word is ', end='')
 
         for letter in mystery_letters:
-                if user_guess == mystery_letters:
+                if user_guess == letter:
                         guessed_letters.append(user_guess)
         
         for letter in mystery_letters:
                 if letter in guessed_letters:
-                        print(letter + " ", end="")
+                        print(letter + ' ', end='')
                 else:
-                        print(" _ ", end="")
+                        print('_ ', end='')
 
-        print(number_of_lives)
 
+        print('-----------------------------------------')
+        
         if len(guessed_letters) == len(mystery_letters):
                 print("You have won, well done!")
                 break
