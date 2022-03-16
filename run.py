8da2ba8ce912958b0bc54d13bfd676b2be38fdf2
johnjmci <mcivorjohnj@gmail.com>
2022-03-16 13:09:27 +0000

    Update to append user inputs to uppercase

--- run.py
+++ run.py
@@ -1,18 +1,9 @@
-"""
-Hangman
-"""
-# Install repository of available words
 import random
 from allwords import possible_words
-# possible_words = ['apple', 'banana', 'pears']
+import string
 
-
-# name_input = input("What is your name?")
-# print(name_input)
 
 # Use username to explain rules to user
-
-# Ask user to press any key to begin
 
 
 # Welcome user and ask for input of first name
@@ -20,41 +11,54 @@ print('Welcome to Hangman!\nThis is a fun word game, where you guess the letters
 print()
 print('In this version of the game, all words have six letters.')
 print()
-username = input('Before we start, what should I call you? Plesae enter your first name... ')
+username = input('Before we start, what should I call you? Plesae enter your first name... ').upper()
+print()
+print('Hi',username, '! Best of luck')
+print()
+
+input('Press any key to continue.')
+print()
 
 wrong_answers = 0
 guessed_letters = []
 number_of_lives = 7
+alphabet = set(string.ascii_uppercase)
 mystery_word = random.choice(possible_words)
 mystery_letters = list(mystery_word)
 wrong_letters = []
 
 
-
-
-
-
-print('The word has {} letters'.format(len(mystery_letters)))
-
+# Display word skeleton and empty gallowes, request first letter guess
 
 
 while wrong_answers < number_of_lives:
-        print('Incorrect answers so far: ', end='')
+        print('* Incorrect answers so far: ', end='')
+        print()
         for letter in wrong_letters:
                 print('{}, '.format(letter), end='')
+        print('* Lives remaining: {}'.format(number_of_lives - wrong_answers))
         print()
-        print('Lives remaining: {}'.format(number_of_lives - wrong_answers))
-        user_guess = input('Enter a letter to check if it is in the mystery word...')
+        user_guess = input('--->>  Enter a letter to check if it is in the mystery word...').upper()
+
+
+
+  # Run code to check if the input is valid - a single letter character from a to z
+
+   ##     print("Invalid character entered, please enter a leter from a to z.")
+
+
+
 
         while user_guess in guessed_letters or user_guess in wrong_letters:
-                print('You already madethat guess, please try again')
-                user_guess = input('Enter a letter to check if it is in the mystery word.')
+                print('You already made that guess, please try again')
+                user_guess = input('--->>  Enter a letter to check if it is in the mystery word...').upper()
 
         if user_guess not in mystery_letters:
                 wrong_answers += 1
                 wrong_letters.append(user_guess)
-
+        print()
         print('MYSTERY Word is ', end='')
+        print()
 
         for letter in mystery_letters:
                 if user_guess == letter:
@@ -67,8 +71,13 @@ while wrong_answers < number_of_lives:
                         print('_ ', end='')
 
 
-        print('-----------------------------------------')
+        print()
         
+
+        # If no lives remain then GAME OVER
+
+# If no letters remain then CONGRATULATIONS
+
         if len(guessed_letters) == len(mystery_letters):
                 print("You have won, well done!")
                 break
@@ -79,45 +88,3 @@ if wrong_answers == number_of_lives:
 
 
 
-
-
-"""
-Will use the imported word list to select a word to be guessed in the game"
-"""
-#def generate_word(possible_words):
-     
-
-#def game():
- #   mystery_word = generate_word(possible_words)
-  #  mystery_letters = set(mystery_word)
-   # alphabet = set(string.ascii_uppercase)
-    #guessed_letters = set()
-#
-
-# Display word skeleton and empty gallowes, request first letter guess
-    
-   
-
-
-
-# isolate user input
-#    user_guess = input("Enter a letter to check if it is in the mystery word.").upper()
-# Check whether guess is contained in word, if yes then congrat user, no add body part and comiserate
-# If lives and letters remain, LOOP code
-# Run code to check if the input is valid - a single letter character from a to z
- ##   if user_guess in alphabet - guessed_letters:
-   ##         guessed_letters.append(user_guess)
- ##   if user_guess in mystery_letters:
-   ##         mystery_letters.remove(user_guess)
-
- ##   elif user_guess in guessed_letters:
-
-  ##  else: 
-   ##     print("Invalid character entered, please enter a leter from a to z.")
-
-
-# If no lives remain then GAME OVER
-
-# If no letters remain then CONGRATULATIONS
-
-## game()
